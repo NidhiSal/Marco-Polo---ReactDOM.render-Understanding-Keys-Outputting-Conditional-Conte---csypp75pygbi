58ac5cf50db114f58330924f45e7f7685ca18f46
NidhiSal <110630480+NidhiSal@users.noreply.github.com>
2022-11-26 23:07:02 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
-import React from 'react'
+// import React from 'react'
+// import '../styles/App.css';
+// const App = () => {
+//   const [count, setCount] = useState(true);
+//   return (
+//     <div id="main">
+//       <h1 id='marco polo'>{setCount}</h1>
+//     </div>
+//   )
+// }
+
+
+// export default App;
+import React, { useState } from 'react'
 import '../styles/App.css';
 const App = () => {
-  
+  const [st, setSt] = useState(true);
+
   return (
     <div id="main">
-
+      <h1 id="marco-polo">{st ? 'Marco' : 'Polo'}</h1>
+      <button id="marco-polo-toggler" onClick={() => setSt(st => !st)}>{st ? 'Polo' : 'Marco'}</button>
     </div>
   )
 }
